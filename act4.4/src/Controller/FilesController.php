e8dbf0a89b758e935bc9d255c08e74a98b7e638a
Kenza Daghrir <kdaghrir@dom.tti>
2022-07-04 15:37:12 +0100

    adding new functions

--- act4.4/src/Controller/FilesController.php
+++ act4.4/src/Controller/FilesController.php
@@ -8,6 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+use Symfony\Component\Filesystem\Exception\InvalidArgumentException;
+use Symfony\Component\Filesystem\Exception\IOException;
+
 class FilesController extends AbstractController
 {
     /**
@@ -38,8 +42,8 @@ $current_dir_path = getcwd();
     {
         $fsObject->touch($new_file_path);
         $fsObject->chmod($new_file_path, 0777);
-         $fsObject->dumpFile($new_file_path, "Adding dummy content to bar.txt file.\n");
-         $fsObject->appendToFile($new_file_path, "This should be added to the end of the file.\n");
+        //  $fsObject->dumpFile($new_file_path, "Adding dummy content to bar.txt file.\n");
+        //  $fsObject->appendToFile($new_file_path, "This should be added to the end of the file.\n");
          $message="new file created";
     }
 
@@ -48,4 +52,71 @@ $current_dir_path = getcwd();
 }
  return new Response($message);
 }
+
+ /**
+      * @Route("/write/{filename}/{text}", name="write_files" )
+      */
+
+    public function Write(string $filename, string $text):Response     {
+   // init file system
+   
+   $fsObject = new Filesystem();
+   $current_dir_path = getcwd();
+   $new_file_path = $current_dir_path . "/$filename.txt";
+    { if (!$fsObject->exists($new_file_path)){
+        !$fsObject->dumpFile($filename, $text);
+       
+    }else{
+        $fsObject->appendToFile($filename, $text);
+    }
+    return new Response($filename . ' is added with the text: ' . $text);
+    }
+    }
+
+/**
+      * @Route("/copy/{filename}/{filename_copy}", name="copy_files" )
+      */
+
+      public function Copyfile(string $filename, string $filename_copy):Response {
+
+    try {
+        $fsObject = new Filesystem();
+        $current_dir_path = getcwd();
+        $src_dir_path = $current_dir_path . "/$filename";
+        $dest_dir_path = $current_dir_path . "/$filename_copy";
+     
+        if (!$fsObject->exists($dest_dir_path))
+        {
+            $fsObject->copy($src_dir_path, $dest_dir_path , true);
+        }
+
+    } catch (IOExceptionInterface $exception) {
+        echo "Error copying directory at". $exception->getPath();
+    }
+    return new Response($filename . ' is COPIED');
+}
+
+
+
+/**
+      * @Route("/delete/{filename}", name="delete_files" )
+      */
+      public function Deletefile(string $filename):Response {
+
+        try {
+            $fsObject = new Filesystem();
+            $current_dir_path = getcwd();
+            $src_dir_path = $current_dir_path . "/$filename";
+       
+         
+            if ($fsObject->exists($src_dir_path))
+            {
+                $fsObject->remove(['symlink', $src_dir_path, "/$filename" ]);
+            }
+    
+        } catch (IOExceptionInterface $exception) {
+            echo "Error copying directory at". $exception->getPath();
+        }
+        return new Response($filename . ' is deleted');
+    }
 }
